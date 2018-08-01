21fb5141b2b6dc07e5668d4475289e4bdd5d65fc
Roman Stankiewicz <roman.stan3@gmail.com>
2018-08-01 10:28:40 +0100

    pwa-ified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import {Route,Switch} from 'react-router-dom'
 import Home from 'pages/Home'
 
 export default class App extends Component {
+  commponentDidMount() {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js').then(registration => {
+          console.log('ServiceWorker registration successful with scope');
+        }, err => {
+          console.log('ServiceWorker registration failed');
+        }).catch(err => {
+          console.log(err)
+        })
+      })
+    } else {
+      console.log('service worker is not supported');
+    }
+  }
+
   render() {
     return (
       <Fragment>
