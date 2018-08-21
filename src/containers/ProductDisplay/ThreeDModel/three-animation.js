a119fe99ae3909bae735943cce6f7c88554d8056
Roman Stankiewicz <roman.stan3@gmail.com>
2018-08-21 14:33:25 +0100

    adjusting lights and swoop in animation of cgi model

--- src/containers/ProductDisplay/ThreeDModel/three-animation.js
+++ src/containers/ProductDisplay/ThreeDModel/three-animation.js
@@ -6,16 +6,30 @@ var THREE = window.THREE
 
 let camera, scene, renderer, frameRequest, geometry, material, cube, controls, mesh
 
-const scale = d3.scaleLinear().domain([250, 1000]).range([2, 50])
+const zScale = d3.scaleLinear().domain([220, 1000]).range([2, 50])
+const yScale = d3.scaleLinear().domain([85, 500]).range([1, 50])
+const xScale = d3.scaleLinear().domain([85, 500]).range([1, 50])
 
 function animate() {
   render()
   frameRequest = requestAnimationFrame(animate)
 
-  if(camera.position.z > 250) {
-    camera.position.z -= scale(camera.position.z)
+  if(camera.position.z > 220) {
+    camera.position.z -= zScale(camera.position.z)
+    camera.position.y -= yScale(camera.position.y)
     camera.lookAt(0,0,0)
   }
+  //
+  // if(camera.position.y > 110 && camera.position.z < 230) {
+  //   // camera.position.z -= zScale(camera.position.z)
+  //   camera.position.y -= yScale(camera.position.y)
+  //   camera.lookAt(0,0,0)
+  // }
+
+  // if(camera.position.z > 220 && camera.position.z < 300 && camera.position.x ) {
+  //   camera.position.z -= xScale(camera.position.z)
+  // }
+
 }
 
 function render() {
@@ -41,20 +55,27 @@ function setCanvasSize() {
 }
 
 function createLights() {
-  let light = new THREE.AmbientLight( 0xffffff, 0.5 );
-  scene.add( light );
+  // let light = new THREE.AmbientLight( 0xffffff, 0.9 );
+  // scene.add( light );
 
-  let light2 = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
-	light2.position.set( 0, 1, 0 );
-	scene.add( light2 );
+  // let light2 = new THREE.HemisphereLight( 0xffffff, 1 );
+	// light2.position.set( 0, 300, 100  );
+	// scene.add( light2 );
 
-  let spotLight = new THREE.SpotLight( 0xffffff, 1.0 );
-  spotLight.position.set( 0, 20, 20 );
-  spotLight.angle = 1;
+  let light3 = new THREE.HemisphereLight( 0xffffff, 1 );
+	light3.position.set( 0, 300, -200  );
+	scene.add( light3 );
+
+  const spotLight = new THREE.SpotLight( 0xffffff, 1.0 );
+  spotLight.position.set( 0, 500, 340 );
+  spotLight.angle = 120;
   spotLight.penumbra = 0.95;
   spotLight.distance = 1000;
 
   spotLight.castShadow = true;
+  spotLight.shadowDarkness = 0.1;
+  // light.shadowCameraVisible = true;
+
   spotLight.shadow.darkness = 0.5;
   spotLight.shadow.mapSize.width = 1024;
   spotLight.shadow.mapSize.height = 1024;
@@ -64,10 +85,12 @@ function createLights() {
   spotLight.shadow.camera.fov = 30;
   spotLight.decay = 0;
 
+  console.log('sl: ', spotLight)
+
   scene.add( spotLight );
 
-  var spotLightHelper = new THREE.SpotLightHelper( spotLight );
-  // scene.add( spotLightHelper );
+  const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+  // scene.add( spotLightHelper )
 }
 
 
@@ -79,15 +102,10 @@ function deviceOrientation(e) {
     if(snapshot) {
       betaChange = e.beta
       gammaChange = e.gamma
-      // gammaChange = e.gamma * (Math.PI / 200)
-      // alphaChange = e.alpha * (Math.PI / 200)
       snapshot = false
     }
-
     cube.rotation.x = ((e.beta - betaChange) * (Math.PI / 900))
     cube.rotation.y = ((e.gamma - gammaChange) * (Math.PI / 400))
-    // cube.rotation.z =  (e.alpha * (Math.PI / 200)) - alphaChange - 0.2
-    // cube.rotation.z =  -0.2
   }
 }
 
@@ -98,17 +116,27 @@ function loadModel(url) {
     const manager = new THREE.LoadingManager()
     manager.onLoad = () => {
       window.addEventListener('deviceorientation', deviceOrientation)
-      camera.position.set( 0, 110, 500 )
+      camera.position.set( 50, 310, 400 )
       camera.lookAt(0,0,0)
     }
     const loader = new THREE.GLTFLoader(manager)
+
     loader.load(url, ( gltf ) => {
       cube = gltf.scene
       cube.name = url
       scene.add(cube)
+      cube.castShadow = true;
+      cube.receiveShadow = true;
       cube.children[0].position.y = 20
       cube.children[0].position.z = 0
       cube.children[0].position.x = 0
+      cube.children[0].traverse(child => {
+				if ( child.isMesh ) {
+					child.castShadow = true;
+					child.receiveShadow = true;
+				}
+			})
+
     })
   },200)
 }
@@ -130,14 +158,38 @@ export function init() {
 
   scene = new THREE.Scene()
   camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 0.85, 1000 )
-	camera.position.set( 0, 110, 500 )
+	camera.position.set( 0, 310, 400 )
 
   // var axesHelper = new THREE.AxesHelper( 700 );
   // scene.add( axesHelper );
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setClearColor( 0xffffff, 1)
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
+
   renderer.setSize( canvas.width, canvas.height )
+  renderer.setPixelRatio( window.devicePixelRatio )
+
+  // const geometry = new THREE.BoxGeometry(30, 30, 30)
+  // const material = new THREE.MeshPhongMaterial({ color: '#433F81' })
+  // const box = new THREE.Mesh(geometry, material)
+  // box.castShadow = true;
+  // box.position.y = 80
+  // box.position.z = 70
+
+  // scene.add(box)
+
+  const floorGeometry = new THREE.BoxGeometry(400, 0.1, 400)
+  // const floorMaterial = new THREE.MeshPhongMaterial({ color: '#433F81' })
+  // const floorMaterial = new THREE.MeshPhongMaterial({ color: '#303c54' })
+  const floorMaterial = new THREE.MeshPhongMaterial({ color: '#ffffff' })
+
+  const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+  floor.position.y = -30
+  floor.receiveShadow = true;
+
+  scene.add(floor)
 
   window.addEventListener("resize", () => {
     canvas = setCanvasSize()
