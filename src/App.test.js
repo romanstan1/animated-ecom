1395c4c77e98c5eef7789de92338d6bbe93e53e8
Roman Stankiewicz <roman.stan3@gmail.com>
2018-09-05 10:53:16 +0100

    testing set-up

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+
+// function sum(a, b) {
+//   return a + b;
+// }
+//
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
